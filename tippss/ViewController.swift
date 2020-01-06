f700323736882dfeb55b450f32dd67e7118bf65e
aanimesh23 <animesha@uci.edu>
2020-01-06 14:28:39 -0800

    Initial Commit

---
+++ tippss/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippss
+//
+//  Created by Animesh Agrawal on 1/6/20.
+//  Copyright © 2020 Animesh Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
